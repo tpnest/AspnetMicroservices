b10fab03deeb6d241763526a83a3d6d972ff9c7e
TP <815546553@qq.com>
2024-03-31 03:27:26 +0800

    创建购物车实体及添加redis

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿namespace Basket.Api.Entities;
+
+public class ShoppingCart
+{
+    /// <summary>
+    /// 用户名
+    /// </summary>
+    public string? UserName { get; set; }
+    /// <summary>
+    /// 购物车商品
+    /// </summary>
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public ShoppingCart() { }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    /// <summary>
+    /// 总价
+    /// </summary>
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal totalPrice = 0;
+            foreach (var item in Items)
+            {
+                totalPrice += item.Price * item.Quantity;
+            }
+            return totalPrice;
+        }
+    }
+}
