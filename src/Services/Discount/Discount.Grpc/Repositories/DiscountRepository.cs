d3dfaed5e6a37c2bf2dc51df0b655fc320ad17ca
tp <815546553@qq.com>
2024-04-02 16:35:30 +0800

    折扣Grpc服务

---
+++ src/Services/Discount/Discount.Grpc/Repositories/DiscountRepository.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using Discount.Grpc.Entities;
+using Npgsql;
+using System.Data;
+
+namespace Discount.Grpc.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            _conn = new NpgsqlConnection(connectionString);
+        }
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            try
+            {
+                var affected = await _conn.ExecuteAsync(
+                    "INSERT INTO Coupon(ProductName,Description,Amount) VALUES(@ProductName,@Description,@Amount)",
+                    new { coupon.ProductName, coupon.Description, coupon.Amount });
+
+                return affected != 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            try
+            {
+                var affected = await _conn.ExecuteAsync(
+                    "DELETE FROM Coupon WHERE ProductName = @productName",
+                    new { productName });
+
+                return affected != 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            try
+            {
+                var coupon = await _conn.QueryFirstOrDefaultAsync<Coupon>(
+                    "SELECT Id, ProductName, Description, Amount FROM Coupon WHERE ProductName = @productName",
+                    new { productName });
+
+                return coupon ?? new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount" };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new Coupon { ProductName = "Error", Amount = 0, Description = "Database error" };
+            }
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            try
+            {
+                var affected = await _conn.ExecuteAsync(
+                    "UPDATE Coupon Set ProductName = @ProductName,Description = @Description, Amount = @Amount WHERE Id = @Id",
+                    new { coupon.ProductName, coupon.Description, coupon.Amount, coupon.Id });
+
+                return affected != 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountServiceImpl.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountServiceImpl : DiscountProtoService.DiscountProtoServiceBase
+{
+    private readonly IDiscountRepository _repository;
+    private readonly ILogger<DiscountServiceImpl> _logger;
+    private readonly IMapper _mapper;
+
+    public DiscountServiceImpl(IDiscountRepository repository, ILogger<DiscountServiceImpl> logger, IMapper mapper)
+    {
+        _repository = repository;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _repository.GetDiscount(request.ProductName);
+        if (coupon == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound,
+                $"Discount with ProductName = {request.ProductName} not found"));
+        }
+        return _mapper.Map<CouponModel>(coupon);
+    }
+}
