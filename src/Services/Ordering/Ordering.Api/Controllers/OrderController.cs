f929c252d090e297333fce92a21a51b6836525eb
TP <815546553@qq.com>
2024-04-15 01:20:52 +0800

    update

---
+++ src/Services/Ordering/Ordering.Api/Controllers/OrderController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+namespace Ordering.Api.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrdersVM>>> GetOrders(string userName)
+        {
+            var query = new GetOrdersListQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var orderId = await _mediator.Send(command);
+            return Ok(orderId);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")] 
+        public async Task<ActionResult> DeleteOrder(int id)
+        {
+            await _mediator.Send(new DeleteOrderCommand { Id = id });
+            return NoContent();
+        }
+    }
+        
+}
--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering API", Version = "v1" });
+});
 
 var app = builder.Build();
 
